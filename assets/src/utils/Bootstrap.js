8d4821a396cfbbf4b9934ec3d2511c7274e7de8e
Asad Nur <asaduzzamannur.dev@gmail.com>
2022-06-23 10:20:12 +0600

    enahance: vue chartjs integration

--- assets/src/utils/Bootstrap.js
+++ assets/src/utils/Bootstrap.js
@@ -12,6 +12,7 @@ import Multiselect from 'vue-multiselect'
 import Modal from './components/Modal.vue'
 import Switches from './components/Switches.vue'
 import "vue-multiselect/dist/vue-multiselect.min.css"
+import * as VueChartJS from 'vue-chartjs'
 
 import dayjs from 'dayjs';
 window.dayjs = dayjs;
@@ -83,6 +84,7 @@ window.wepos.libs['TextEditor'] = TextEditor;
 window.wepos.libs['EventBus']   = EventBus;
 window.wepos.libs['Modal']      = Modal;
 window.wepos.libs['Switches']   = Switches;
+window.wepos.libs['VueChartJS'] = VueChartJS;
 
 // WordPress Hooks
 import { createHooks } from '@wordpress/hooks';
